3a2a84f783c90f8804a689992bc506e0e6a2c77b
shubhamclear <shubham.kothavade09@gmail.com>
2024-03-07 23:33:36 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import InputBox from './Component/InputBox'
 
 const App = () => {
   // hello
+  // hello new codeed to check how to merge
   return (
     <div>
       <InputBox placeholder={"enter your name"} type={submit}/>
